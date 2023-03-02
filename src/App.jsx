31b4b452a3e6a27f7175172be0fffa8f0bed898f
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2023-03-02 12:58:17 +0100

    started project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+import Header from './components/header/Header'
+
+function App() {
+
+  return (
+    <div className="bg-black h-screen">
+      <header className='h-16 bg-[#313131]'>
+        <Header/>
+      </header>
+    </div>
+  )
+}
+
+export default App
