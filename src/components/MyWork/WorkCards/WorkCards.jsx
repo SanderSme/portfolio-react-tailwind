5ec2afbe6c2502e902f301cb0644814760144a60
Sander Smedbøl <sander.smedbol@stud.noroff.no>
2023-03-02 17:56:34 +0100

    add fonts

--- src/components/MyWork/WorkCards/WorkCards.jsx
+++ src/components/MyWork/WorkCards/WorkCards.jsx
@@ -7,7 +7,7 @@ const WorkCards = ({img, altText, heading, info, netlify, github}) => {
       <div className='w-full md:w-[45%] my-2 md:my-8 p-8 bg-[#4d4d4d10] rounded-lg shadow-lg flex flex-col justify-between'>
           <img src={img} alt={altText} className='w-full'/>
           <div>
-          <h2 className=' text-center text-xl font-semibold my-2'>{heading}</h2>
+          <h2 className=' text-center text-xl md:text-2xl font-semibold my-2'>{heading}</h2>
           <p className='text-sm md:text-base'>{info}</p>
           <div className='w-full flex justify-between items-center mt-2'>
               <a href={netlify} className='flex gap-2 items-center py-1 md:py-2 px-2 md:px-4 bg-[#313131] hover:bg-black text-gray-100 rounded-lg text-xs'><img src={netlifyLogo} className='w-[23px]'/> Visit site</a>
